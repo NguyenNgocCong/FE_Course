e3c7190bd5e214bee6291b63d8ca24aa2eb24b09
toanhhg123 <toanhhg123@gmail.com>
2022-11-26 21:18:30 +0700

    redux auth store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import blogSlice from "./reducers/blog";
 import userSlice from "./reducers/user";
+import authSlice from "./reducers/auth";
 
 export const store = configureStore({
   reducer: {
     userReducers: userSlice.reducer,
     blogReducers: blogSlice.reducer,
+    auth: authSlice.reducer,
   },
 });
